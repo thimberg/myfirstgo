b72f170f498448ad7db8fae20a535524a6df22b8
thimberg <baoxingbo@my-first-instance-1.c.endless-fire-177814.internal>
2017-09-26 01:10:46 +0000

    version up

--- main/news6park.go
+++ main/news6park.go
@@ -18,19 +18,45 @@ func getNewsContent(url string) (string, error) {
 	}
 	
 	newsCont := ""
-	doc.Find("section.item").Each(func(_ int, s *goquery.Selection) {
+	doc.Find("div#mainContent").Each(func(_ int, s *goquery.Selection) {
 		content,_ := iconv.ConvertString(s.Text(), "gb2312", "utf-8")
 		newsCont = newsCont + content
 	})
 	
 	return newsCont, nil
 }
+const (
+    targetfqdn = "github-ranking.com"
+)
+
+func IsRelativePath(url string) bool {
+    if strings.Index(url, "//") == 0 {
+        return false
+    } else if strings.Index(url, "/") == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+func GetAbsoluteURLFromRelativePath(scheme string, fqdn string, relativePath string) string {
+    return scheme + "://" + fqdn + relativePath
+}
+
+
+func hasNextPageURL(doc *goquery.Document) (string, bool) {
+    nexturl, exists := doc.Find("div > section.item > a").First().Attr("href")
+    if exists == true && IsRelativePath(nexturl) {
+        return GetAbsoluteURLFromRelativePath("https", targetfqdn, nexturl), true
+    }
+    return nexturl, false
+}
 
 func main() {
 	//メイン処理のループ間隔(秒)
 	const sleepInterval = 1000
 
-	const news6parkUrl = "https://m.6park.com/" 
+	const news6parkUrl = "http://www.6park.com/" 
 
 	// DB接続
 	db, err := sql.Open("mysql", "wpuser:asdffdsa1234@/wp_myblog")
@@ -61,6 +87,11 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
+//		if(exist == true ) {
+//			fmt.Println(newurl)
+//		} else {
+//			fmt.Println("not find")
+//		}
 
 		//ランダムに取得したwikipediaのタイトルと取得日時を表示する
 ///		doc.Find("head").Each(func(i int, s *goquery.Selection) {
@@ -74,19 +105,21 @@ func main() {
 ///			fmt.Println(strings.Repeat("☆", 50))
 ///		})
 
+
 		//divのidを目印に説明文を取得して表示する
-		doc.Find("section.item").Each(func(i int, s *goquery.Selection) {
-			fmt.Println(s)
+		doc.Find("div#parknews > a").Each(func(i int, s *goquery.Selection) {
+///			fmt.Println(s)
+
+
 			url, _ := s.Attr("href")
+
 			if strings.HasPrefix(url, "./index.php") {
 				url = news6parkUrl+ fmt.Sprint(url[2:len(url)])
 			}
 			
 			fmt.Println(url)
-//			html, _ := s.Html()
-//			fmt.Println(html)
-			s.Find("p").Each(func(i int, s *goquery.Selection) {
-				
+//			s.Find("p").Each(func(i int, s *goquery.Selection) {
+//				
 //				utf8Txt := make([]byte, len(gb2312Txt))
 //				utf8Txt = utf8Txt[:]	
 //				iconv.Convert(gb2312Txt, utf8Txt, "gb2312", "utf-8")	
@@ -113,7 +146,7 @@ func main() {
 					panic(err.Error())
 				}
 */
-			})
+///			})
 
 			fmt.Println()
 			
